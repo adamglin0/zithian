c2ed918968347812e8cc52fdc8754ab36c8100df
adamglin <wwalkingg@gmail.com>
2025-04-28 10:08:13 +0800

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+@file:Suppress("UnstableApiUsage")
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "zithian"
