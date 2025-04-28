4f90732115081948e75b47e060bdf2b7a89f20a9
adamglin <wwalkingg@gmail.com>
2025-04-28 11:00:49 +0800

    Add initial project setup with Android, Kotlin, and Compose
    This commit introduces the base configuration for the project. It includes Android, Kotlin Multiplatform, and Compose support, alongside Gradle settings and module structure. Essential Gradle properties, plugins, and dependencies are defined for cross-platform and UI development.

---
+++ ui/build.gradle.kts
@@ -0,0 +1,59 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.mavenPublish)
+    alias(libs.plugins.binaryCompatibilityValidator)
+}
+
+kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_1_8
+        }
+    }
+    jvm {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_1_8
+        }
+    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    macosX64()
+    macosArm64()
+
+    js(compiler = IR) {
+        browser()
+    }
+
+    wasmJs {
+        browser()
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.ui)
+        }
+    }
+}
+
+android {
+    namespace = "com.adamglin.zithian.ui"
+    compileSdk = libs.versions.androidCompileSdk.get().toInt()
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+
+    buildFeatures {
+        compose = true
+    }
+}
