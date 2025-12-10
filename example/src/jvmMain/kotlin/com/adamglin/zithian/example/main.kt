aecfb03e0f2c3e1a7c717501317c533366d416fc
adamglin <wwalkingg@gmail.com>
2025-12-10 21:49:05 +0800

    Refactor imports and enable diagnostic stack trace mode in the main application

--- example/src/jvmMain/kotlin/com/adamglin/zithian/example/main.kt
+++ example/src/jvmMain/kotlin/com/adamglin/zithian/example/main.kt
@@ -2,46 +2,25 @@ package com.adamglin.zithian.example
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
 import androidx.compose.runtime.retain.retain
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.tooling.ComposeStackTraceMode
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.fastForEach
-import androidx.compose.ui.window.Popup
-import androidx.compose.ui.window.PopupProperties
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import androidx.compose.ui.window.singleWindowApplication
 import com.adamglin.zithian.compose.layout.VerticalDivider
 import com.adamglin.zithian.compose.text.Text
 import com.adamglin.zithian.compose.theme.InteractType
 import com.adamglin.zithian.compose.theme.ZithianTheme
 import com.adamglin.zithian.compose.theme.platformDefault
-import com.adamglin.zithian.example.screens.screens.Buttons
-import com.adamglin.zithian.example.screens.screens.Home
-import com.adamglin.zithian.example.screens.screens.Inputs
-import com.adamglin.zithian.example.screens.screens.Other
-import com.adamglin.zithian.example.screens.screens.Texts
-import com.adamglin.zithian.example.screens.screens.Sheets
-import kotlinx.coroutines.delay
+import com.adamglin.zithian.example.screens.screens.*
 
+@OptIn(ExperimentalComposeRuntimeApi::class)
 fun main() {
+    Composer.setDiagnosticStackTraceMode(ComposeStackTraceMode.SourceInformation)
     application {
         Window(onCloseRequest = ::exitApplication) {
             var interactType by remember { mutableStateOf(InteractType.platformDefault) }
