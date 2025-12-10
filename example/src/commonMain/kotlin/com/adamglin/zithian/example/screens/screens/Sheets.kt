135b0deaf93208148fd0b143d0ae366d23ac01b8
adamglin <wwalkingg@gmail.com>
2025-12-10 12:49:42 +0800

    Refactor `Sheets` screen: update header and content styling, add `BasicFiller` and improve structure

--- example/src/commonMain/kotlin/com/adamglin/zithian/example/screens/screens/Sheets.kt
+++ example/src/commonMain/kotlin/com/adamglin/zithian/example/screens/screens/Sheets.kt
@@ -1,19 +1,15 @@
 package com.adamglin.zithian.example.screens.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.adamglin.zithian.compose.button.PrimaryButton
+import com.adamglin.zithian.compose.layout.BasicFiller
 import com.adamglin.zithian.compose.sheets.BasicSheet
 import com.adamglin.zithian.compose.text.Text
+import com.adamglin.zithian.compose.theme.ZithianTheme
 
 @Composable
 fun Sheets() {
@@ -31,10 +27,36 @@ fun Sheets() {
             isVisible = isSheetVisible,
             onDismissRequest = { isSheetVisible = false },
             header = {
-                Text("Sheet Header", modifier = Modifier.padding(20.dp))
+                Text(
+                    "Sheet Preview",
+                    style = ZithianTheme.typography.titleSmall,
+                    modifier = Modifier.padding(20.dp)
+                )
             }
         ) {
-            Text("Sheet Content", modifier = Modifier.padding(20.dp))
+            Column(modifier = Modifier.fillMaxWidth()) {
+                BasicFiller(height = headerHeight)
+                Text(
+                    text = """
+                        In the early hush before the day,
+                        Shadows drift and fade away.
+                        A single light begins to rise,
+                        Painting gold across the skies,
+                        Whispering softly: hope will stay.
+                        In the early hush before the day,
+                        Shadows drift and fade away.
+                        A single light begins to rise,
+                        Painting gold across the skies,
+                        Whispering softly: hope will stay.
+                        In the early hush before the day,
+                        Shadows drift and fade away.
+                        A single light begins to rise,
+                        Painting gold across the skies,
+                        Whispering softly: hope will stay.
+                    """.trimIndent(),
+                    modifier = Modifier.padding(20.dp)
+                )
+            }
         }
     }
 }
