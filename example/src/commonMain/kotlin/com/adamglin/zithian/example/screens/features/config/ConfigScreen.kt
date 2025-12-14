c569f6bb053b1fa1706f1a1d62db8525c4aaace6
adamglin <wwalkingg@gmail.com>
2025-12-14 14:02:40 +0800

    Refactor `ConfigScreen` to use `SimpleTitleAndCloseBottomSheet`, introduce reusable bottom sheet component, and add password entry dialog with `BasicDialog`.

--- example/src/commonMain/kotlin/com/adamglin/zithian/example/screens/features/config/ConfigScreen.kt
+++ example/src/commonMain/kotlin/com/adamglin/zithian/example/screens/features/config/ConfigScreen.kt
@@ -1,17 +1,22 @@
 package com.adamglin.zithian.example.screens.features.config
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.adamglin.zithian.compose.button.NeutralButton
 import com.adamglin.zithian.compose.button.PrimaryButton
+import com.adamglin.zithian.compose.dialog.BasicDialog
 import com.adamglin.zithian.compose.layout.BasicFiller
 import com.adamglin.zithian.compose.scaffold.ScreenScaffold
-import com.adamglin.zithian.compose.sheets.BottomSheet
-import com.adamglin.zithian.compose.sheets.header.TitleAndCloseSheetTitle
 import com.adamglin.zithian.compose.text.Text
+import com.adamglin.zithian.compose.textfield.OutlinedTextField
+import com.adamglin.zithian.compose.theme.ZithianTheme
 import com.adamglin.zithian.example.screens.LocalAppState
 import com.adamglin.zithian.example.screens.LocalExampleAppFontFamily
+import com.adamglin.zithian.example.screens.features.config.sheets.SimpleTitleAndCloseBottomSheet
 import com.adamglin.zithian.example.screens.features.liquid_test.LiquidTestNavKey
 import com.adamglin.zithian.example.screens.widgets.SimpleTextTopBar
 import com.adamglin.zithian.example.screens.widgets.TopLevelSharableBottomNavigation
@@ -43,17 +48,46 @@ fun ConfigScreen() {
             ) { Text("Bottom Sheet") }
         }
     }
-
-    BottomSheet(
-        isVisible = isExampleBottomSheetVisible,
-        onDismissRequest = { isExampleBottomSheetVisible = false },
-        header = {
-            TitleAndCloseSheetTitle(
-                title = { Text("BottomSheet Example", fontFamily = LocalExampleAppFontFamily.current.montserrat) },
-                onClose = { isExampleBottomSheetVisible = false }
-            )
-        }
+    SimpleTitleAndCloseBottomSheet(
+        isExampleBottomSheetVisible,
+        onDismissRequest = { isExampleBottomSheetVisible = false }
+    )
+    BasicDialog(
+        isVisible = true,
     ) {
-        Box(modifier = Modifier.fillMaxWidth().height(400.dp))
+        Column(modifier = Modifier.background(ZithianTheme.colors.surface).padding(20.dp)) {
+            Column(modifier = Modifier.padding(10.dp)) {
+                Text(
+                    text = "Enter Password",
+                    fontFamily = LocalExampleAppFontFamily.current.montserrat,
+                    style = ZithianTheme.typography.titleMedium
+                )
+                BasicFiller(height = 8.dp)
+                Text(
+                    text = "To proceed, please enter your password for adamglin.com",
+                    style = ZithianTheme.typography.bodyMedium,
+                    color = ZithianTheme.colors.text7
+                )
+                BasicFiller(height = 10.dp)
+                OutlinedTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    state = rememberTextFieldState()
+                )
+            }
+            BasicFiller(height = 0.dp)
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                NeutralButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = {}
+                ) { Text("Cancel") }
+                PrimaryButton(
+                    modifier = Modifier.weight(1f),
+                    onClick = {}
+                ) { Text("OK") }
+            }
+        }
     }
 }
---
+++ example/src/commonMain/kotlin/com/adamglin/zithian/example/screens/features/config/sheets/SimpleTitleAndCloseBottomSheet.kt
@@ -0,0 +1,31 @@
+package com.adamglin.zithian.example.screens.features.config.sheets
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.adamglin.zithian.compose.sheets.BottomSheet
+import com.adamglin.zithian.compose.sheets.header.TitleAndCloseSheetTitle
+import com.adamglin.zithian.compose.text.Text
+import com.adamglin.zithian.example.screens.LocalExampleAppFontFamily
+
+@Composable
+fun SimpleTitleAndCloseBottomSheet(
+    isExampleBottomSheetVisible: Boolean,
+    onDismissRequest: () -> Unit
+) {
+    BottomSheet(
+        isVisible = isExampleBottomSheetVisible,
+        onDismissRequest = { onDismissRequest() },
+        header = {
+            TitleAndCloseSheetTitle(
+                title = { Text("BottomSheet Example", fontFamily = LocalExampleAppFontFamily.current.montserrat) },
+                onClose = { onDismissRequest() }
+            )
+        }
+    ) {
+        Box(modifier = Modifier.fillMaxWidth().height(400.dp))
+    }
+}
