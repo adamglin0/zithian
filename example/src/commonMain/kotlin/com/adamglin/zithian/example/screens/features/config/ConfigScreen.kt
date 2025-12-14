bcc72070465c205a8d52198b54ea46e5034ae267
adamglin <wwalkingg@gmail.com>
2025-12-14 14:35:53 +0800

    Replace `SheetTitleHeader` with `TitleAndCloseSheetTitle`.
    This change updates all occurrences of the `SheetTitleHeader` composable to use the new `TitleAndCloseSheetTitle` composable. The old implementation has been removed, ensuring consistent use of the updated component across the project.

--- example/src/commonMain/kotlin/com/adamglin/zithian/example/screens/features/config/ConfigScreen.kt
+++ example/src/commonMain/kotlin/com/adamglin/zithian/example/screens/features/config/ConfigScreen.kt
@@ -1,21 +1,16 @@
 package com.adamglin.zithian.example.screens.features.config
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.adamglin.zithian.compose.button.NeutralButton
 import com.adamglin.zithian.compose.button.PrimaryButton
-import com.adamglin.zithian.compose.dialog.BasicDialog
 import com.adamglin.zithian.compose.layout.BasicFiller
 import com.adamglin.zithian.compose.scaffold.ScreenScaffold
 import com.adamglin.zithian.compose.text.Text
-import com.adamglin.zithian.compose.textfield.OutlinedTextField
-import com.adamglin.zithian.compose.theme.ZithianTheme
 import com.adamglin.zithian.example.screens.LocalAppState
-import com.adamglin.zithian.example.screens.LocalExampleAppFontFamily
 import com.adamglin.zithian.example.screens.features.config.sheets.SimpleTitleAndCloseBottomSheet
 import com.adamglin.zithian.example.screens.features.liquid_test.LiquidTestNavKey
 import com.adamglin.zithian.example.screens.widgets.SimpleTextTopBar
@@ -52,42 +47,4 @@ fun ConfigScreen() {
         isExampleBottomSheetVisible,
         onDismissRequest = { isExampleBottomSheetVisible = false }
     )
-    BasicDialog(
-        isVisible = true,
-    ) {
-        Column(modifier = Modifier.background(ZithianTheme.colors.surface).padding(20.dp)) {
-            Column(modifier = Modifier.padding(10.dp)) {
-                Text(
-                    text = "Enter Password",
-                    fontFamily = LocalExampleAppFontFamily.current.montserrat,
-                    style = ZithianTheme.typography.titleMedium
-                )
-                BasicFiller(height = 8.dp)
-                Text(
-                    text = "To proceed, please enter your password for adamglin.com",
-                    style = ZithianTheme.typography.bodyMedium,
-                    color = ZithianTheme.colors.text7
-                )
-                BasicFiller(height = 10.dp)
-                OutlinedTextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    state = rememberTextFieldState()
-                )
-            }
-            BasicFiller(height = 0.dp)
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                NeutralButton(
-                    modifier = Modifier.weight(1f),
-                    onClick = {}
-                ) { Text("Cancel") }
-                PrimaryButton(
-                    modifier = Modifier.weight(1f),
-                    onClick = {}
-                ) { Text("OK") }
-            }
-        }
-    }
 }
