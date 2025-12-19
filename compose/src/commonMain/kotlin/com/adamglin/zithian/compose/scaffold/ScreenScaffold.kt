a87faac268317cdeac8ea40c53164c581b9eefc9
adamglin <wwalkingg@gmail.com>
2025-12-19 11:09:59 +0800

    Wrap `ScreenScaffold` with `CompositionLocalProvider` for window corner radius injection via `LocalSheetContainerRadius`.

--- compose/src/commonMain/kotlin/com/adamglin/zithian/compose/scaffold/ScreenScaffold.kt
+++ compose/src/commonMain/kotlin/com/adamglin/zithian/compose/scaffold/ScreenScaffold.kt
@@ -2,6 +2,7 @@ package com.adamglin.zithian.compose.scaffold
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.dropShadow
 import androidx.compose.ui.geometry.Offset
@@ -9,6 +10,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.adamglin.composecontinuousroundedcornershape.ContinuousRoundedCornerShape
 import com.adamglin.zithian.compose.theme.ZithianTheme
+import com.adamglin.zithian.compose.utils.Device
 
 @Composable
 fun ScreenScaffold(
@@ -19,18 +21,22 @@ fun ScreenScaffold(
     content: @Composable ScaffoldScope.() -> Unit,
 ) {
     val shadowColor = ZithianTheme.colors.shadow
-    BasicScaffold(
-        modifier = Modifier
-            .dropShadow(ContinuousRoundedCornerShape(20.dp)) {
-                offset = Offset(x = -125f, y = 10f)
-                color = shadowColor
-                spread = -17f
-                radius = 250f
-            }
-            .fillMaxSize().then(modifier),
-        header = header,
-        bottom = bottom,
-        backgroundColor = backgroundColor,
-        content = content,
-    )
+    CompositionLocalProvider(
+        LocalSheetContainerRadius provides Device.windowRoundedCornerSize
+    ) {
+        BasicScaffold(
+            modifier = Modifier
+                .dropShadow(ContinuousRoundedCornerShape(20.dp)) {
+                    offset = Offset(x = -125f, y = 10f)
+                    color = shadowColor
+                    spread = -17f
+                    radius = 250f
+                }
+                .fillMaxSize().then(modifier),
+            header = header,
+            bottom = bottom,
+            backgroundColor = backgroundColor,
+            content = content,
+        )
+    }
 }
